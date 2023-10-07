a39ea14529cc557c66f64cee39ad5e557bcb10d8
Vander <vandercaneppelle@outlook.com>
2023-10-07 15:15:32 -0300

    view record page added

--- website/views.py
+++ website/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
 
 def home(request):
+    records = Record.objects.all()
+
     # check if user is logged in
     if request.method == 'POST':
         username = request.POST['username']
@@ -22,7 +25,7 @@ def home(request):
                 request, 'There was an error Logging In, please try again.')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 # def login_user(request):
@@ -52,3 +55,14 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+
+    if request.user.is_authenticated:
+        # look up records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be logged in to views that page")
+        return redirect('home')
